d0230145db6e4d91f0b481e067ed2f71db6ac3a0
shwethakulal <shwetha.hosangadi@gmail.com>
2018-01-06 11:57:46 +0530

    Add project files.

---
+++ JsonObjectExample/Color.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonObjectExample
+{
+    class Color
+    {
+        public string name { get; set; }
+        public string category { get; set; }
+        public string type { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Color: {0} \nCategory: {1} \nType:{2}",name, category,type);
+           
+        }
+    }
+}
---
+++ JsonObjectExample/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.IO;
+
+namespace JsonObjectExample
+{
+    class Program
+    {
+        public static void displayColorDetails(Color color)
+        {
+            Console.WriteLine("Color name:{0}",color.name);
+            Console.WriteLine("Color category:{0}", color.category);
+            Console.WriteLine("Color type:{0}", color.type);
+        }
+        static void Main(string[] args)
+        {
+           
+            string jsondata = File.ReadAllText(@"data\JSONdata.json");
+
+            JavaScriptSerializer ser = new JavaScriptSerializer();
+
+            List<Color> colors = ser.Deserialize<List<Color>>(jsondata);
+            foreach (Color color in colors)
+            {
+                displayColorDetails(color);
+               
+            }
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
